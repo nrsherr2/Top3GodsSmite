384ffbc8d0fec57cc52293702adba3a8fbf82745
Nicholas Sherrill <nrsherr2@ncsu.edu>
2018-03-06 15:04:15 -0500

    #1 working on getting page source

--- Top3Gods/src/Selenium/T3Driver.java
+++ Top3Gods/src/Selenium/T3Driver.java
@@ -1,21 +1,33 @@
 package Selenium;
 
-import java.util.Scanner;
 
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import util.NameWeightList;
 
 public class T3Driver {
 
-    public String[][] getTop3 ( String player ) {
+    public static String[][] getTop3 ( String player ) throws InterruptedException {
+        NameWeightList n = new NameWeightList();
+        String url = "http://smite.guru/profile/pc/" + player + "/matches";
 
-        String url = "smite.guru/profile/pc/" + player + "/matches";
-        WebDriver driver = new ChromeDriver();
+        String fName = "";
+        WebDriver driver = new HtmlUnitDriver();
+
         driver.get( url );
+        Thread.sleep( 500 );
         for ( int i = 0; i < 3; i++ ) {
             try {
                 String src = driver.getPageSource();
-                Scanner pageSource = new Scanner( src );
+                // Scanner pageSource = new Scanner( src );
+                System.out.println( src );
             }
             catch ( Exception e ) {
                 e.printStackTrace();
--- Top3Gods/src/ui/ConsoleUI.java
+++ Top3Gods/src/ui/ConsoleUI.java
@@ -2,9 +2,11 @@ package ui;
 
 import java.util.Scanner;
 
+import Selenium.T3Driver;
+
 public class ConsoleUI {
 
-    public static void main ( String[] args ) {
+    public static void main ( String[] args ) throws InterruptedException {
         System.out.println( "Enter the names of every player you want to look up:" );
         Scanner in = new Scanner( System.in );
         String ln = in.nextLine();
@@ -13,11 +15,12 @@ public class ConsoleUI {
         for ( int i = 0; i < players.length; i++ ) {
             String[][] results = new String[3][2]; // replace this with a call
                                                    // to our driver class
-            System.out.println( "For player " + players[i] + ":" );
+            T3Driver.getTop3( players[i] );
+            /*System.out.println( "For player " + players[i] + ":" );
             for ( int j = 0; j < 3; j++ ) {
                 System.out.println( " " + j + ". " + results[j][0] + " - " + results[j][1] );
             }
-            System.out.println();
+            System.out.println();*/
         }
         in.close();
     }
