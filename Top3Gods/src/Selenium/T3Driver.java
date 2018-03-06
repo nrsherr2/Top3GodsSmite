55c537ac22544f784cc3cc3b67630974864347bb
Nicholas Sherrill <nrsherr2@ncsu.edu>
2018-03-06 12:20:13 -0500

    #1 starting on the driver class

---
+++ Top3Gods/src/Selenium/T3Driver.java
@@ -0,0 +1,15 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class T3Driver {
+
+    public String[][] getTop3(String player){
+        
+        String url = "smite.guru/profile/pc/" + player + "/matches";
+        WebDriver driver = new ChromeDriver();
+        driver.get( url );
+        return null;
+    }
+}
--- Top3Gods/src/ui/ConsoleUI.java
+++ Top3Gods/src/ui/ConsoleUI.java
@@ -6,16 +6,18 @@ public class ConsoleUI {
 
     public static void main ( String[] args ) {
         System.out.println( "Enter the names of every player you want to look up:" );
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner( System.in );
         String ln = in.nextLine();
         String[] players = ln.split( " " );
 
-        for(int i = 0; i < players.length; i++) {
-            String[][] results = new String[3][2]; //replace this with a call to our driver class
+        for ( int i = 0; i < players.length; i++ ) {
+            String[][] results = new String[3][2]; // replace this with a call
+                                                   // to our driver class
             System.out.println( "For player " + players[i] + ":" );
-            for (int j = 0; j < 3; j++) {
+            for ( int j = 0; j < 3; j++ ) {
                 System.out.println( " " + j + ". " + results[j][0] + " - " + results[j][1] );
             }
+            System.out.println();
         }
         in.close();
     }
